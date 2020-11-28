4629965d1b79d52af4c5a6d0a8b46877db7ef9ae
mr.khavari <mr.khavari.n@gmail.com>
2020-11-28 10:21:25 +0330

    solving puzzle with Special Goal added

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,10 +7,13 @@
 #include <sstream>
 #include <cmath>
 
-std::vector<Node*> solve_puzzle(std::shared_ptr<Node> node);
+std::vector<Node*> solve_puzzle(std::shared_ptr<Node> node ,int goal_id = 123456780);
 void solve_random_puzzle();
 void solve_your_puzzle();
 void solve_puzzle_by_your_self();
+void solve_random_puzzle_with_special_goal();
+void solve_your_puzzle_with_special_goal();
+
 
 
 int main()
@@ -21,9 +24,11 @@ int main()
         std::cout << "\033[1m\033[30m" <<  "----------------------------------------" << "\033[0m" << std::endl;
         std::cout << "\033[1m\033[36m" << "You can execute one of the following commands" << std::endl;
         std::cout << "Enter the command number for this operation" << "\033[0m" << std::endl;
-        std::cout << "\033[1m\033[35m" << "1 : Solve a Random Puzzle" << std::endl;
-        std::cout << "2 : Solve Your Puzzle" << std::endl;
-        std::cout << "3 : Solve the puzzle by yourself" << std::endl;
+        std::cout << "\033[1m\033[35m" << "1 : Solving A Random Puzzle" << std::endl;
+        std::cout << "2 : Solving Your Puzzle" << std::endl;
+        std::cout << "3 : Solve The Puzzle By Yourself" << std::endl;
+        std::cout << "4 : Solving A Random Puzzle With Special Goal" << std::endl;
+        std::cout << "5 : Solving Your Puzzle With Special Goal" << std::endl;
         std::cout << "0 : Exit " << "\033[0m" << std::endl;
         std::cout << "\033[1m\033[34m" << "\nWhats Your Command : ";
         int command;
@@ -41,6 +46,14 @@ int main()
         {
             solve_puzzle_by_your_self();
         }
+        if (command == 4)
+        {
+            solve_random_puzzle_with_special_goal();
+        }
+        if (command == 5)
+        {
+            solve_your_puzzle_with_special_goal();
+        }
         if (command == 0)
         {
             std::cout << "\033[0m";
@@ -49,9 +62,8 @@ int main()
     }
 }
 
-std::vector<Node*> solve_puzzle(std::shared_ptr<Node> node)
+std::vector<Node*> solve_puzzle(std::shared_ptr<Node> node , int goal_id)
 {   
-    int goal_id = 123456780; 
     std::vector<Node*> order;
     node->make_childs();
     std::vector<int> all_id;
@@ -231,9 +243,9 @@ void solve_puzzle_by_your_self()
     std::time_t start = time(0);
     while( difftime(std::time(0), start) <=1.5);
     std::cout << "You can change the puzzle with the following guide" << "\033[0m" << std::endl;
-    std::cout << "\033[1m\033[33m" << "         UP         \n         8         " << std::endl;
-    std::cout << "LEFT 4         6 RIGHT" << std::endl;
-    std::cout << "         2     \n        down         " << "\033[0m" << std::endl;
+    std::cout << "\033[1m\033[33m" << "            UP         \n           8,w         " << std::endl;
+    std::cout << "LEFT 4,a         d,6 RIGHT" << std::endl;
+    std::cout << "           2,s     \n          DOWN         " << "\033[0m" << std::endl;
     std::cout << "\033[1m\033[36m" << "For example, if you select down, the number above the blank field will go down" << std::endl;
     std::cout << "If you are tired, enter 0" << std::endl;
     std::time_t start1 = time(0);
@@ -259,15 +271,15 @@ void solve_puzzle_by_your_self()
         node->show();
         std::cout << "\033[1m\033[30m" << "------------------------" << "\033[0m" << std::endl;
         std::cout << "\033[1m\033[34m" << "Choose a move : ";
-        int cm;
+        std::string cm;
         std::cin >> cm;
         std::cout << "\033[0m";
-        if (cm == 0)
+        if (cm == "0")
         {
             std::cout << "\033[1m\033[32m" << "\n-------Done--------" << "\033[0m" << std::endl;
             return;
         }
-        else if (cm == 8)
+        else if (cm == "8" || cm == "w")
         {
             if (node->zero_row == 2)
             {
@@ -280,7 +292,7 @@ void solve_puzzle_by_your_self()
                 node->make_id();
             }
         }
-        else if (cm == 4)
+        else if (cm == "4" || cm == "a")
         {
             if (node->zero_column == 2)
             {
@@ -293,7 +305,7 @@ void solve_puzzle_by_your_self()
                 node->make_id();
             }
         }
-        else if (cm == 6)
+        else if (cm == "6" || cm == "d")
         {
             if (node->zero_column == 0)
             {
@@ -306,7 +318,7 @@ void solve_puzzle_by_your_self()
                 node->make_id();
             }
         }
-        else if (cm == 2)
+        else if (cm == "2" || cm == "s")
         {
             if (node->zero_row == 0)
             {
@@ -324,4 +336,153 @@ void solve_puzzle_by_your_self()
             std::cout << "\033[1m\033[31m" << "This command is not available" << "\033[0m" << std::endl;
         }
     }
+}
+
+void solve_random_puzzle_with_special_goal()
+{
+    std::string pattern;
+    std::cout << "\033[1m\033[37m" << "for example" << std::endl;
+    std::cout << "5   3   2 \n1   8   6  ------> 532186704\n7       4" << "\033[0m" << std::endl;
+    std::cout << "\033[1m\033[34m" << "You must enter your Special Goal : ";
+    std::cin >> pattern;
+    std::cout << "\033[0m";
+    std::stringstream geek(pattern);
+    int goal_id; 
+    geek >> goal_id;
+
+    int goal_id_n = goal_id;
+    std::vector<std::vector<int>> members;
+    int pow{8};
+    for (int i{0} ; i < 3 ; i++)
+    {
+        std::vector<int> row;
+        for (int j{0} ; j < 3 ; j++)
+        {
+            int p = std::pow(10,pow);
+            int num = int(goal_id_n/p);
+            goal_id_n = goal_id_n - (p*num);
+            row.push_back(num);
+            pow--;
+        }
+        members.push_back(row);
+    }
+    
+    std::shared_ptr<Node> goal_node{std::make_shared<Node>(members)};
+
+    std::shared_ptr<Node> node{std::make_shared<Node>()};
+    node->random();
+    std::cout << "\033[1m\033[36m" <<  "I chose a random puzzle"<< "\n" << "\033[0m" << std::endl;
+    node->show();
+    std::vector<Node*> order;
+    order = solve_puzzle(node,goal_id);
+    if (order.size() == 0)
+    {
+        std::cout << "\033[1m\033[31m" << "This Puzzle is not solvable" << "\033[0m" << std::endl;
+    }
+    else
+    {
+        std::cout << "\033[1m\033[32m" << "I solved it" << "\n" << "\033[0m" << std::endl;
+        for (size_t n{1} ; n < order.size() ; n++)
+        {
+            std::time_t start = time(0);
+            while( difftime(std::time(0), start) <=0.5 );
+            std::cout << "\033[1m\033[30m" << "\n" << "--------------" << "\033[0m" << std::endl;
+            std::cout << "\033[1m\033[34m" << "Step : " << n << "\n" << "\033[0m" << std::endl;
+            order[order.size()-n-1]->show();
+        }
+        std::time_t start = time(0);
+        while( difftime(std::time(0), start) <=0.5 );
+        std::cout << "\033[1m\033[30m" << "\n" << "--------------" << "\033[0m" << std::endl;
+        std::cout << "\033[1m\033[34m" << "Step : " << order.size() << "\n" << "\033[0m" << std::endl;
+        goal_node->show();
+    }
+    std::cout << "\033[1m\033[32m" << "\n-------Done--------" << "\033[0m" << std::endl;
+    std::time_t start = time(0);
+    while( difftime(std::time(0), start) <=1);
+}
+
+void solve_your_puzzle_with_special_goal()
+{
+    std::string pattern;
+    std::cout << "\033[1m\033[37m" << "for example" << std::endl;
+    std::cout << "5   3   2 \n1   8   6  ------> 532186704\n7       4" << "\033[0m" << std::endl;
+    std::cout << "\033[1m\033[34m" << "You must enter your puzzle pattern : ";
+    std::cin >> pattern;
+    std::cout << "\033[0m";
+    std::stringstream geek(pattern);
+    int id; 
+    geek >> id;
+
+    std::vector<std::vector<int>> members;
+    int pow{8};
+    for (int i{0} ; i < 3 ; i++)
+    {
+        std::vector<int> row;
+        for (int j{0} ; j < 3 ; j++)
+        {
+            int p = std::pow(10,pow);
+            int num = int(id/p);
+            id = id - (p*num);
+            row.push_back(num);
+            pow--;
+        }
+        members.push_back(row);
+    }
+    
+    std::shared_ptr<Node> node{std::make_shared<Node>(members)};
+    std::cout << "\033[1m\033[36m" << "This is your puzzle and I want to solve it" << "\n" << "\033[0m" << std::endl;
+    node->show();
+
+    std::string pattern_n;
+    std::cout << "\033[1m\033[34m" << "You must enter your Special Goal : ";
+    std::cin >> pattern_n;
+    std::cout << "\033[0m";
+    std::stringstream geek_n(pattern_n);
+    int goal_id; 
+    geek_n >> goal_id;
+
+    int goal_id_n = goal_id;
+    std::vector<std::vector<int>> members_n;
+    int pow_n{8};
+    for (int i{0} ; i < 3 ; i++)
+    {
+        std::vector<int> row;
+        for (int j{0} ; j < 3 ; j++)
+        {
+            int p = std::pow(10,pow_n);
+            int num = int(goal_id_n/p);
+            goal_id_n = goal_id_n - (p*num);
+            row.push_back(num);
+            pow_n--;
+        }
+        members_n.push_back(row);
+    }
+    std::shared_ptr<Node> goal_node{std::make_shared<Node>(members_n)};
+
+    std::vector<Node*> order;
+    order = solve_puzzle(node,goal_id);
+    if (order.size() == 0)
+    {
+        std::cout << "\033[1m\033[31m" << "This Puzzle is not solvable" << "\033[0m" << std::endl;
+    }
+    else
+    {
+        std::cout << "\033[1m\033[32m" << "I solved it" << "\n" << "\033[0m" << std::endl;
+        for (size_t n{1} ; n < order.size() ; n++)
+        {
+            std::time_t start = time(0);
+            while( difftime(std::time(0), start) <=0.5 );
+            std::cout << "\033[1m\033[30m" << "\n" << "--------------" << "\033[0m" << std::endl;
+            std::cout << "\033[1m\033[34m" << "Step : " << n << "\n" << "\033[0m" << std::endl;
+            order[order.size()-n-1]->show();
+        }
+        std::time_t start = time(0);
+        while( difftime(std::time(0), start) <=0.5 );
+        std::cout << "\033[1m\033[30m" << "\n" << "--------------" << "\033[0m" << std::endl;
+        std::cout << "\033[1m\033[34m" << "Step : " << order.size() << "\n" << "\033[0m" << std::endl;
+        goal_node->show();
+    }
+    std::cout << "\033[1m\033[32m" << "\n-------Done--------" << "\033[0m" << std::endl;
+    std::time_t start = time(0);
+    while( difftime(std::time(0), start) <=1);
 }
